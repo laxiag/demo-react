0537d1b29639bce41eab103c5c2d0c87c8cdc50c
ecity_lanxiang <lanxiang@ecitychina.com>
2019-05-21 11:43:36 +0800

    make next expand plan.

--- src/sell/north.js
+++ src/sell/north.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class North extends React.PureComponent {
   render(){
     return (
-      <div>north sell is built.</div>
+      <div>north sell is built. And next step is to expand south market.</div>
     );
   }
 }
